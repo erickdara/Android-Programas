276bce795a68805e0be303e27b3f49da34bab3ac
ErickRangel <erick1rg@gmail.com>
2020-09-28 15:35:01 -0500

    Mis programas JAVA

--- Asteroides/app/src/main/java/com/erickrangel/asteroides/MainActivity.java
+++ Asteroides/app/src/main/java/com/erickrangel/asteroides/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,5 +25,12 @@ public class MainActivity extends AppCompatActivity {
     public void salirApp (View view){
 
         finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_en_activity, menu);
+        return true;
     }
 }
